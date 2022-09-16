846e39e76255ad1d42c6e52ad6bf7be9c670f96a
snow4dv <log.mihail2003@gmail.com>
2022-09-16 17:31:13 +0300

    Third practice w/o finished test classes

---
+++ practice-3/src/ShapeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShapeTest {
+
+    @org.junit.jupiter.api.Test
+    void setFilled() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPerimeter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
