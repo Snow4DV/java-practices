ef615c887a4991f17e0cdbbf60701151f8aab00b
snowadv <log.mihail2003@gmail.com>
2022-09-24 16:33:24 +0300

    practice-5 created

---
+++ practice-5/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    // Ex 16
+    private static int exercise16(int max) {
+return 0;
+    }
+}
