7babd0099057e4638f48b5a8294918f412e206ca
snow4dv <log.mihail2003@gmail.com>
2022-10-01 17:42:58 +0300

    Practice 6 2/3

--- practice-6/src/StudentSorter.java
+++ practice-6/src/StudentSorter.java
@@ -1,9 +1,11 @@
+import java.util.Comparator;
+
 public class StudentSorter {
-    public static void insertionSort(Student[] array) {
+    public static <T extends Comparable<T>> void insertionSort(T[] array) {
         for (int sortedLength = 1; sortedLength < array.length; sortedLength++) {
             int index = sortedLength;
             while(index != 0 && array[index].compareTo(array[index - 1]) < 0) {
-                Student temp = array[index];
+                T temp = array[index];
                 array[index] = array[index-1];
                 array[index-1] = temp;
                 index --;
@@ -11,7 +13,37 @@ public class StudentSorter {
         }
     }
 
-    public static Student[] quickSort(Student[] array) {
-        if()
+    public static <T extends Comparable<T>> void quickSort(Comparator<T> comparator, T[] array, int firstI , int lastI) {
+
+
+        if(firstI < lastI) {
+            int pivot = (firstI + lastI)/2;
+
+            int left = firstI, right = lastI;
+
+            do {
+                while (comparator.compare(array[left], array[pivot]) > 0) {
+                    left++;
+                }
+
+                while (comparator.compare(array[right], array[pivot]) < 0) {
+                    right--;
+                }
+
+                if (left <= right) {
+                    if (left < right) {
+                        T temp = array[left];
+                        array[left] = array[right];
+                        array[right] = temp;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+            while(left <= right);
+            int divIndex = (firstI + lastI)/2;
+            quickSort(comparator, array, firstI, divIndex);
+            quickSort(comparator, array, divIndex+1, lastI);
+        }
     }
 }
