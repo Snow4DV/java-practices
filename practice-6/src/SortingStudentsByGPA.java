5764a3f543f855843dec62a313272bca981f159e
m1ke <m1ke@DESKTOP-01BDDA.localdomain>
2022-10-14 19:01:02 +0300

    Clean repo

--- practice-6/src/SortingStudentsByGPA.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortingStudentsByGPA implements Comparator<Student> {
-    @Override
-    public int compare(Student o1, Student o2) {
-        return o1.getGradePoints() - o2.getGradePoints();
-    }
-}
